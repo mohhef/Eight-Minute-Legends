ec1cc290a454681a4b160aef357fd99a9c0698c9
mohhef <mohhef@rocketmail.com>
2021-02-05 20:18:09 -0500

    allow for the validation of checking continent linking

--- src/Map/Map.h
+++ src/Map/Map.h
@@ -14,6 +14,7 @@ struct Continent {
     string *name;
     Continent(string name) {
         this -> name = new string(name);
+
     }
 };
 
@@ -35,10 +36,16 @@ public:
     // a vector that has all the continents and its regions
     vector< pair <Continent*, vector<Region*>>> *continents;
 
+    vector<pair<Region *, bool>>* getNeighbourList(Region *region);
+
     void addRegion(Region *region);
+    void addContinent(Continent *continent);
     //use true if path is land, else false
     void addPath(Region *start, Region *destination,bool land);
     void displayMap();
+    bool isValid();
+    bool areRegionsConnected();
+    bool areContinentsConnected();
 };
 
 
