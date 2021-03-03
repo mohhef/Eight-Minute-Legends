8986d96baa93f725d7589cc16daa6a5e70c60656
mohhef <mohhef@rocketmail.com>
2021-03-03 18:38:50 -0500

    add all action functions

--- src/Player/Player.h
+++ src/Player/Player.h
@@ -20,8 +20,6 @@ class Player {
   Hand *hand;
   pair<Region *, int> *GetArmiesInRegion(Region *region);
   pair<Region *, int> *GetCitiesInRegion(Region *region);
-  bool MoveOverLand(int armies_num, Region *origin, Region *destination);
-  bool MoveOverWater(int armies_num, Region *origin, Region *destination);
 
  public:
   Player(Map *map, string name, int cubes_num, int discs_num, int coins_num);
@@ -44,4 +42,7 @@ class Player {
   bool BuildCity(Region *region);
   bool DestroyArmy(Player *player, Region *region);
   bool MoveArmies(int armies_num, Region *origin, Region *destination);
+  bool MoveOverLand(int armies_num, Region *origin, Region *destination);
+  bool MoveOverWater(int armies_num, Region *origin, Region *destination);
+
 };
