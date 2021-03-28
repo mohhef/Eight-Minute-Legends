23af1800620553ad974197c0b394798c3ebd8f44
mohhef <mohhef@rocketmail.com>
2021-03-28 16:29:52 -0400

    adding comments

--- src/Setup/SetupDriver.cpp
+++ src/Setup/SetupDriver.cpp
@@ -8,10 +8,16 @@ using namespace std;
 
 void Driver::part6() {
   Setup *setup = new Setup();
+  //p1 (load map)
   setup->loadGame();
+  //p1 (2 player game, each player has a bidding facility, game components)
   setup->initializePlayers();
+  //p1 (deck created) //p2 (shuffle deck in deck constructor)
   setup->initializeDeck();
+  //p2 (resource assignment, bidding and assigning who will start)
   setup->Startup();
+  //p3 (running game loop, pay for cards, display of each player card) //p4(methods) //p5 happens within this main loop
   setup->mainLoop();
+  //p6 (computing the score)
   setup->computeScore();
 }
