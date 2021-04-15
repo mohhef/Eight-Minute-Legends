08a4ec100292b4711f4f05cb9336f2d868cdf11f
Maxim Pobudzey <maxim.pobudzey@net4user.com>
2021-04-15 12:14:19 -0400

    Added tracking of conquered regions and continents

--- src/Setup/Setup.h
+++ src/Setup/Setup.h
@@ -4,24 +4,24 @@
 
 #pragma once
 
+#include <map>
+
 #include "../Cards/Cards.h"
 #include "../Map/Map.h"
 #include "../MapLoader/MapLoader.h"
 #include "../Player/Player.h"
-#include "../Cards/Cards.h"
-#include <map>
 #include "GameObservers.h"
 
-enum State { pickCard, showBoard};
+enum State { pickCard, showBoard };
 
-class Setup : public Subject{
+class Setup : public Subject {
  public:
   Setup();
   ~Setup();
   void deepCopy(const Setup &obj);
   Setup(const Setup &setup);
-  friend ostream &operator<<(ostream& os, const Setup& setup);
-  Setup& operator=(const Setup setup);
+  friend ostream &operator<<(ostream &os, const Setup &setup);
+  Setup &operator=(const Setup setup);
 
   void changeState(State state);
   void loadGame();
@@ -32,6 +32,9 @@ class Setup : public Subject{
   int computeScore();
   bool checkGameOver();
   void takeTurn(Player *player, int turn);
+  void UpdateConquerings();
+  void ConquerRegion(Player &player, Region &region);
+  void ConquerContinent(Player &player, Continent &continent);
 
   Player *findPlayer(string playerName);
   bool andOrAction(Player &player, Cards &card);
@@ -50,15 +53,17 @@ class Setup : public Subject{
   Player *current_player;
   Cards *selected_card;
   int *current_cost;
+  vector<pair<Region *, Player *>> *conquered_regions;
+  vector<pair<Continent *, Player *>> *conquered_continents;
 };
 
-
-class TurnView : public Observer{
+class TurnView : public Observer {
  public:
   TurnView();
-  TurnView(Setup* s);
+  TurnView(Setup *s);
   void update();
   void display();
+
  private:
   Setup *subject;
 };
